8db438c92237cdfc96ac63d35fcfa11fde9b4b3f
unknown <priyanka0003a@gmail.com>
2026-02-12 17:34:11 +0530

    Updated API base URL for production

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-baseURL: import.meta.env.VITE_API_URL,
+baseURL: "https://signature-app-backend-5uvj.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
